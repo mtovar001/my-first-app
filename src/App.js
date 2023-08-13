c7459dced130a6582af31a18534941c260b83d8b
Mildred Tovar <tovarmildred2020@gmail.com>
2023-08-13 13:35:44 -0400

    adding images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
+import bimage from './assets/sdDGgeneric.png';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>       
+      <img className='image' src={bimage} alt="My image profile"/>
+      <Nav/>     
     </div>
   );
 }
